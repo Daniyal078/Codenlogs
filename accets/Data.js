4f82d02fe369bec0a4ca0914e1edc8965a92104f
Muhammad Danyial <syeddaniyalali078@email.com>
2025-01-20 02:38:26 +0500

    add

---
+++ accets/Data.js
@@ -0,0 +1,277 @@
+let website = [
+    {
+        id: "one",
+        img: "accets/images/Logo Portfolio/Wordmark/5.jpg"
+    },
+    {
+        id: "two",
+        img: "accets/images/comicpops.jpg"
+    },
+    {
+        id: "three",
+        img: "accets/images/img-453049.jpg"
+    },
+    {
+        id: "",
+        img: "accets/images/website portfolio/Dispute-landing-page.jpg"
+    },
+    {
+        id: "",
+        img: "accets/images/website portfolio/Dispute-landing-page.jpg"
+    },
+    {
+        id: "",
+        img: "accets/images/website portfolio/home v1 – 1.png"
+    },
+    {
+        img: "accets/images/website portfolio/home v1 – 1.png"
+    },
+    {
+        img: "accets/images/website portfolio/Homepage 2.png"
+    },
+    {
+        img: "accets/images/website portfolio/Homepage.png"
+    },
+    {
+        img: "accets/images/website portfolio/opl-master-12.jpg"
+    },
+    {
+        img: "accets/images/website portfolio/Upstate-Gold-Burger-Marinade.jpg"
+    },
+]
+
+
+
+
+let logos = {
+    Abstract: [
+        {
+            img: "accets/images/Logo Portfolio/Abstract Logos/1.jpg"
+        },
+        {
+            img: "accets/images/Logo Portfolio/Abstract Logos/2.jpg"
+        },
+        {
+            img: "accets/images/Logo Portfolio/Abstract Logos/3.jpg"
+        },
+        {
+            img: "accets/images/Logo Portfolio/Abstract Logos/4.jpg"
+        },
+        {
+            img: "accets/images/Logo Portfolio/Abstract Logos/5.jpg"
+        },
+        {
+            img: "accets/images/Logo Portfolio/Abstract Logos/8.jpg"
+        },
+        {
+            img: "accets/images/Logo Portfolio/Abstract Logos/9.jpg"
+        },
+        {
+            img: "accets/images/Logo Portfolio/Abstract Logos/99.jpg"
+        },
+    ],
+
+    iconic: [
+        {
+            img: "accets/images/Logo Portfolio/Iconic/1.jpg"
+        },
+        {
+            img: "accets/images/Logo Portfolio/Iconic/4.jpg"
+        },
+        {
+            img: "accets/images/Logo Portfolio/Iconic/6.jpg"
+        },
+        {
+            img: "accets/images/Logo Portfolio/Iconic/8.jpg"
+        },
+        {
+            img: "accets/images/Logo Portfolio/Iconic/9.jpg"
+        },
+        {
+            img: "accets/images/Logo Portfolio/Iconic/11.jpg"
+        },
+        {
+            img: "accets/images/Logo Portfolio/Iconic/12.jpg"
+        },
+        {
+            img: "accets/images/Logo Portfolio/Iconic/15.jpg"
+        },
+        {
+            img: "accets/images/Logo Portfolio/Iconic/44.jpg"
+        },
+        {
+            img: "accets/images/Logo Portfolio/Iconic/999.jpg"
+        },
+
+    ],
+
+    illustration: [
+        {
+            img: "accets/images/Logo Portfolio/illustration/1.jpg"
+        },
+        {
+            img: "accets/images/Logo Portfolio/illustration/2.jpg"
+        },
+        {
+            img: "accets/images/Logo Portfolio/illustration/4.jpg"
+        },
+        {
+            img: "accets/images/Logo Portfolio/illustration/5.jpg"
+        },
+        {
+            img: "accets/images/Logo Portfolio/illustration/8.jpg"
+        },
+        {
+            img: "accets/images/Logo Portfolio/illustration/10.jpg"
+        },
+        {
+            img: "accets/images/Logo Portfolio/illustration/13.jpg"
+        },
+        {
+            img: "accets/images/Logo Portfolio/illustration/15.jpg"
+        },
+        {
+            img: "accets/images/Logo Portfolio/illustration/44.jpg"
+        },
+        {
+            img: "accets/images/Logo Portfolio/illustration/99.jpg"
+        },
+
+    ],
+    
+    Mascot: [
+        {
+            img: "accets/images/Logo Portfolio/Mascot/1.jpg"
+        },
+        {
+            img: "accets/images/Logo Portfolio/Mascot/5.jpg"
+        },
+        {
+            img: "accets/images/Logo Portfolio/Mascot/6.jpg"
+        },
+        {
+            img: "accets/images/Logo Portfolio/Mascot/10.jpg"
+        },
+        {
+            img: "accets/images/Logo Portfolio/Mascot/11.jpg"
+        },
+        {
+            img: "accets/images/Logo Portfolio/Mascot/12.jpg"
+        },
+        {
+            img: "accets/images/Logo Portfolio/Mascot/13.jpg"
+        },
+        {
+            img: "accets/images/Logo Portfolio/Mascot/15.jpg"
+        },
+       
+    ],
+    
+    Minimal: [
+        {
+            img: "accets/images/Logo Portfolio/Minimal/4.jpg"
+        },
+        {
+            img: "accets/images/Logo Portfolio/Minimal/5.jpg"
+        },
+        {
+            img: "accets/images/Logo Portfolio/Minimal/6.jpg"
+        },
+        {
+            img: "accets/images/Logo Portfolio/Minimal/7.jpg"
+        },
+        {
+            img: "accets/images/Logo Portfolio/Minimal/9.jpg"
+        },
+        {
+            img: "accets/images/Logo Portfolio/Minimal/11.jpg"
+        },
+        {
+            img: "accets/images/Logo Portfolio/Minimal/14.jpg"
+        },
+        {
+            img: "accets/images/Logo Portfolio/Minimal/15.jpg"
+        },
+        {
+            img: "accets/images/Logo Portfolio/Minimal/44.jpg"
+        },
+       
+    ],
+    
+    MonogramBadge: [
+        {
+            img: "accets/images/Logo Portfolio/Monogram, Badge/1.jpg"
+        },
+        {
+            img: "accets/images/Logo Portfolio/Monogram, Badge/6.jpg"
+        },
+        {
+            img: "accets/images/Logo Portfolio/Monogram, Badge/8.jpg"
+        },
+        {
+            img: "accets/images/Logo Portfolio/Monogram, Badge/10.jpg"
+        },
+        {
+            img: "accets/images/Logo Portfolio/Monogram, Badge/11.jpg"
+        },
+        {
+            img: "accets/images/Logo Portfolio/Monogram, Badge/12.jpg"
+        },
+        {
+            img: "accets/images/Logo Portfolio/Monogram, Badge/13.jpg"
+        },
+        {
+            img: "accets/images/Logo Portfolio/Monogram, Badge/1111.jpg"
+        },
+       
+    ],
+
+    Vector3d: [
+        {
+            img: "accets/images/Logo Portfolio/Vector 3d/1.jpg"
+        },
+        {
+            img: "accets/images/Logo Portfolio/Vector 3d/4.jpg"
+        },
+        {
+            img: "accets/images/Logo Portfolio/Vector 3d/7.jpg"
+        },
+        {
+            img: "accets/images/Logo Portfolio/Vector 3d/9.jpg"
+        },
+        {
+            img: "accets/images/Logo Portfolio/Vector 3d/11.jpg"
+        },
+        {
+            img: "accets/images/Logo Portfolio/Vector 3d/13.jpg"
+        },
+        {
+            img: "accets/images/Logo Portfolio/Vector 3d/15.jpg"
+        },       
+    ],
+    
+    Wordmark: [
+        {
+            img: "accets/images/Logo Portfolio/Wordmark/2.jpg"
+        },
+        {
+            img: "accets/images/Logo Portfolio/Wordmark/5.jpg"
+        },
+        {
+            img: "accets/images/Logo Portfolio/Wordmark/6.jpg"
+        },
+        {
+            img: "accets/images/Logo Portfolio/Wordmark/8.jpg"
+        },
+        {
+            img: "accets/images/Logo Portfolio/Wordmark/9.jpg"
+        },
+        {
+            img: "accets/images/Logo Portfolio/Wordmark/10.jpg"
+        },
+        {
+            img: "accets/images/Logo Portfolio/Wordmark/14.jpg"
+        },
+        
+    ],
+}
